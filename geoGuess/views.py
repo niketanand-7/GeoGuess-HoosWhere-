086a5e5d5af198cf72ccd24d2700d39b3a821c22
Kyle Tran <yzn3hv@virginia.edu>
2023-10-06 22:33:07 -0400

    Updating the View a little, fixing some issues with login

--- geoGuess/views.py
+++ geoGuess/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+class Home(TemplateView):
+    template_name = "home.html"
 
 # Create your views here.
 def index(request):
