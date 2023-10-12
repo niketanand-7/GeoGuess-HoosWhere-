0e6fcda30018a6cf138e5d1903b4b61ae40cecb0
Ravi J <spy7sw@virginia.edu>
2023-10-11 23:02:11 -0400

    Added check to user login in maps

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-b=vdh)_px21+ro8myxovt^#bojyo&my2mpdt!hr+*6*&z6++tr'
 
 SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY",
