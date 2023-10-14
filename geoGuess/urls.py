0c3899fc604109f5c45b139de134652f8a2dad41
treiter317 <mvy3ns@virginia.edu>
2023-10-13 21:16:28 -0400

    added basic bootstrap navbar

--- geoGuess/urls.py
+++ geoGuess/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     #path('', TemplateView.as_view(template_name="index.html")),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
-    path('maps/', views.maps_view, name = "maps_view")
+    path('maps/', views.maps_view, name = "challenge")
 ]
