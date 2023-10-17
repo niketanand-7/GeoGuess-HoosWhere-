16b0d3a5e9c19d2105db7f9b777734d0442e65d0
Kyle Tran <yzn3hv@virginia.edu>
2023-10-17 12:27:18 -0400

    Working on Adding a Street View for the Challenge

--- geoGuess/tests.py
+++ geoGuess/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from django.test import Client
+from django.urls import reverse
 # Create your tests here.
+class HomeTestCases(TestCase):
+    def test_google_login_redirect(self):
+        c = Client()
+        google_oauth_url = reverse('Home')
+        response = c.get(google_oauth_url)
+        assert response.status_code == 302
+
