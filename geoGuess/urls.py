133f75ff251dc314ea0178b3b4b8e42287e83315
treiter317 <mvy3ns@virginia.edu>
2023-10-21 09:26:02 -0400

    added basic admin page for seeing all users

--- geoGuess/urls.py
+++ geoGuess/urls.py
@@ -5,11 +5,14 @@ from django.contrib.auth.views import LogoutView
 
 from .import views
 from .views import Home
+from .views import AdminUsersView
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     #path('', TemplateView.as_view(template_name="index.html")),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
-    path('maps/', views.maps_view, name = "challenge")
+    path('maps/', views.maps_view, name = "challenge"),
+    # path('admin/challenge_edit', name="admin_challenge_edit"),
+    path('admin_users/', AdminUsersView.as_view(),name="admin_users"),
 ]
--- geoGuess/views.py
+++ geoGuess/views.py
@@ -1,19 +1,34 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
+from django.views import generic
 from django.shortcuts import render
 import os
+
+# def is_superuser(user):
+#     return user.is_superuser
 
 class Home(TemplateView):
     template_name = "home.html"
 
 # Create your views here.
+@login_required
 def index(request):
     return HttpResponse("Hello, you are at the geoguessing homepage.")
 
-
-
+@login_required
 def maps_view(request):
     context = {
         'maps_api_key': os.environ.get('GOOGLE_MAPS_API_KEY')
     }
     return render(request, 'maps.html', context)
+
+class AdminUsersView(generic.ListView):
+    template_name = "admin/users.html"
+    context_object_name = "user_list"
+    
+    #get all google registered users
+    def get_queryset(self):
+        return User.objects.all()
