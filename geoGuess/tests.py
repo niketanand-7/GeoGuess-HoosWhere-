5f0be96a7b562ebdf059b8c3ec46ba0274ca204b
niketanand-7 <niketanand7@gmail.com>
2023-10-21 22:28:15 -0400

    more test cases involved with logging in

--- geoGuess/tests.py
+++ geoGuess/tests.py
@@ -1,24 +1,39 @@
-from django.test import TestCase
-from django.test import Client
+from django.test import TestCase, Client
 from django.urls import reverse
-# Create your tests here.
 
 class HomeViewTest(TestCase):
+
+    def setUp(self):
+        # Create a test client instance to simulate requests
+        self.client = Client()
+        self.home_url = reverse('home')
+
+    def test_home_view_unauthenticated(self):
+        response = self.client.get(self.home_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Login with Google')  # Ensure login button is visible
+
+    def test_home_view_authenticated(self):
+        # Here, you'd simulate a user being logged in. 
+        # This could be by using the `login` method of the test client or by mocking a user session.
+        # The specifics would depend on how `django-allauth` integrates with Django's authentication.
+        # This is a placeholder and might need adjustments based on your setup.
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(self.home_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Welcome,')  # Ensure user-specific content is visible
+
+    # This is your existing test case, unchanged
     def test_home_view(self):
-        client = Client()
-        response = client.get(reverse('home'))
+        response = self.client.get(self.home_url)
         self.assertEqual(response.status_code, 200)
 
-# class ChoiceViewTest(TestCase):
-#     def test_form_submission(self):
-#         client = Client()
-#         data = {'field_name': 'value', ...}  # Fill with form data
-#         response = client.post(reverse('choice_view_name'), data=data)  # Use the correct URL pattern name for Choice_View
-#         self.assertEqual(response.status_code, 302)  # Assuming you're redirecting after a successful post
-
 class MapsViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
     def test_maps_view(self):
-        client = Client()
-        response = client.get(reverse('challenge'))  # Use the correct URL pattern name for maps_view
+        response = self.client.get(reverse('challenge'))  # Assuming 'challenge' is the correct URL pattern name for maps_view
         self.assertEqual(response.status_code, 200)
         self.assertIn('maps_api_key', response.context)
