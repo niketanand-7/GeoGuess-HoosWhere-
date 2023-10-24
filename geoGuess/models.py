e0c85c6865a13a955b82eba43d0a7b90c7db9c45
niketanand-7 <niketanand7@gmail.com>
2023-10-24 19:24:32 -0400

    challenge model

--- geoGuess/models.py
+++ geoGuess/models.py
@@ -1,8 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Location(models.Model):
     place = models.CharField(max_length=200)
     
     def __str__(self):
         return self.place
+    
+
+
+class Challenge(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to the user who created the challenge
+    image = models.ImageField(upload_to='challenges/')        # Store the uploaded image
+    longitude = models.FloatField()
+    latitutde = models.FloatField()               # Store the answer coordinates
+    timestamp = models.DateTimeField(auto_now_add=True)        # Automatically set when the challenge is created
+
+    def __str__(self):
+        return f"Challenge {self.pk} by {self.user.username}"
+
+    class Meta:
+        verbose_name = "Challenge"
+        verbose_name_plural = "Challenges"
+
     
