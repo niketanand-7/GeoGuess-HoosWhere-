b9e3e54f7614690fa3ee8bc0f3ac0902209264a2
niketanand-7 <niketanand7@gmail.com>
2023-10-24 19:28:53 -0400

    added challengeform

--- geoGuess/forms.py
+++ geoGuess/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Location
+from .models import Location, Challenge
 
 class LocationForm(forms.ModelForm):
     class Meta:
         model = Location
         fields = ['place']
-    
+
+class ChallengeForm(forms.ModelForm):
+    class Meta:
+        model = Challenge
+        fields = ['image', 'longitude', 'latitude']
