a57935e2b559dfc91d48f53fff30fef238a24dad
Ravi J <spy7sw@virginia.edu>
2023-11-04 00:07:35 -0400

    added cron service to generate challenges daily

--- geoGuess/cron.py
+++ geoGuess/cron.py
@@ -1,3 +1,9 @@
 from .models import Challenge
+import math
 def generate_daily_challenge():
     challenge_list = Challenge.objects.filter(approve_status=True)
+    if len(challenge_list) > 0:
+        challenge = challenge_list[math.random(0, challenge_list.count())]
+        return challenge
+    else:
+        return None
