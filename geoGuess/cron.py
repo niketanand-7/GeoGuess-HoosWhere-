2e5f68e6f4013fb8b3e5294404174f964d3673cd
Ravi J <spy7sw@virginia.edu>
2023-11-04 10:29:44 -0400

    Fixed generate_daily_challenge() function

--- geoGuess/cron.py
+++ geoGuess/cron.py
@@ -1,9 +1,15 @@
-from .models import Challenge
-import math
+from .models import Challenge, DailyChallenge
+from random import randint
 def generate_daily_challenge():
     challenge_list = Challenge.objects.filter(approve_status=True)
+    daily_challenge_list = DailyChallenge.objects.all()
+
+    # Remove challenges that have already been used
+    for daily_challenge in daily_challenge_list:
+        challenge_list = challenge_list.exclude(id=daily_challenge.challenge.id)
+    
     if len(challenge_list) > 0:
-        challenge = challenge_list[math.random(0, challenge_list.count())]
+        challenge = challenge_list[randint(0, challenge_list.count() - 1)]
         return challenge
     else:
         return None
